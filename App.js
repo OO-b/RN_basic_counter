7e414b997b082e038d42a6955877e6b193339fb5
OO-b <baeiiyy@gmail.com>
2021-07-11 16:01:23 +0900

    counter

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  //변수선언 
+  //여기서 선언하면 지역변수, export 바깥에서 선언하면 전역변수
+
+  //var cnt = 0;
+
+  // hook : 변수선언과 비슷하나, 값을 바꿔서 넣을 수 있음
+  const [ cnt, setCnt ] = useState(10);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.assa}>카운트 리액트 네이티브</Text>
+      <Text style={styles.cnt}>{cnt}</Text>
+      <Button title="더하기" onPress={()=>setCnt(cnt+1)}></Button>
+      <Button title="빼기" onPress={()=>setCnt(cnt-1)}></Button>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +31,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  assa:{
+    color:'#f00',
+    fontSize:20
+  },
+  cnt:{
+    fontSize:50,
+    marginTop:20
+  }
 });
